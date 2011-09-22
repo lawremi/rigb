6e3c599ff719823a836342f4820908a34bb3c597
Peter Danenberg <pcd@roxygen.org>
2011-09-21 17:21:02 -0700

    No HashMap

--- src/com/gene/bcb/rigb/Activator.java
+++ src/com/gene/bcb/rigb/Activator.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.HashMap;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
